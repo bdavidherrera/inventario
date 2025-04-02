59687d646c8c0e39b12e93e9a6a5f255b8610dcb
Tu Nombre <bherrerabarajs@gmail.com>
2025-04-01 20:03:35 -0500

    enrutador y contoladores

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 /*importamos al framework express */
 
 import express from "express";
+import categoriasRoutes from "./routers/categorias.routes.js"
 
 /*Asignamos a app toda funcionalidad para mi server web */
 
@@ -8,5 +9,9 @@ const app = express();
 
 /*setear un puerto ami web server */
 app.set("port",5000);
+
+/*routers */
+
+app.use("/api/categorias",categoriasRoutes)
 
 export default app;
